85a1832778f4ec58f6e65b2844fceb0eb684d438
bdh0506 <brandon0506@yahoo.com>
2022-10-17 16:23:17 -0700

    updated skills section alignment

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -21,18 +21,46 @@ function Home() {
         </div>
       </div>
       <div className="row skills-wrapper mb-5">
-        <div className="container">
-          <div className="row text-center skills-box">
-            <div className="col-md-6 col-sm-12">
-              &#x2022; attribute1
-              <br />
-              &#x2022; attribute2
+        <div className="container text-center skills-box">
+          <div className="row text-center">
+            <div className="col-xl-12 mb-3">
+              <h2>Skills</h2>
             </div>
-            <div className="col-md-6 col-sm-12">
-              &#x2022; attribute1
-              <br />
-              &#x2022; attribute2
+          </div>
+          <div className="row">
+            <div className="col-sm-6 col-xs-12">
+              <ul>
+                <li>C#</li>
+                <li>Javascript</li>
+                <li>Jquery</li>
+                <li>AJAX/REST</li>
+                <li>Salesforce MC</li>
+                <li>Luminate</li>
+              </ul>
             </div>
+            <div className="col-sm-6 col-xs-12">
+              <ul>
+                <li>.NET/.NET Core</li>
+                <li>React</li>
+                <li>AMPscript</li>
+                <li>SQL</li>
+                <li>Responsys</li>
+                <li>Charity Engine</li>
+              </ul>
+            </div>
+
+            {/* <div className="col-md-6 col-sm-12">&#x2022; C#</div>
+            <div className="col-md-6 col-sm-12 ">&#x2022; .NET/.NET Core</div>
+            <div className="col-md-6 col-sm-12">&#x2022; Javascript</div>
+            <div className="col-md-6 col-sm-12">&#x2022; React</div>
+            <div className="col-md-6 col-sm-12">&#x2022; JQuery</div>
+            <div className="col-md-6 col-sm-12">&#x2022; AMPscript</div>
+            <div className="col-md-6 col-sm-12">&#x2022; AJAX/REST</div>
+            <div className="col-md-6 col-sm-12">&#x2022; SQL</div>
+            <div className="col-md-6 col-sm-12">&#x2022; Salesforce MC</div>
+            <div className="col-md-6 col-sm-12">&#x2022; Responsys</div>
+            <div className="col-md-6 col-sm-12">&#x2022; Luminate</div>
+            <div className="col-md-6 col-sm-12">&#x2022; Charity Engine</div> */}
           </div>
         </div>
       </div>

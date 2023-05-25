88d11e1ede7ad6611c8eea542f67dad93c1ae582
bdh0506 <brandon0506@yahoo.com>
2023-05-25 11:32:10 -0700

    Update resume and home page
    added vetsource to resume
    Update description on home page

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,21 +9,21 @@ function Home() {
         </div>
         <div className="bio-text col-xl-6 col-lg-12">
           Hello, I&#39;m Brandon Harmon and I&#39;m a Senior Developer. Allow me
-          to tell you a little bit about myself, I&#39;m currently working as a
-          Senior Email Developer for Merkle Inc. I have been a professional
-          developer for 6 years but I&#39;ve been developing software and
-          building PC&#39;s and electronics for the last 16 years. Front-end
-          development is my preferred workspace but I am capable of doing full
-          stack work. I follow the motto that you should work to live, not live
-          to work and so I try to find opportunities that support that
-          lifestyle. When I&#39;m not at work I love to play PC games and board
-          games. Working from home has allowed me to cultivate a growing
-          collection of house plants. When time permits I love to get out into
-          the garage and start a new woodworking project. If the weather is
-          nice, you can find me outside with my partner and dogs going on a
-          hike, although, you won&#39;t find me scaling Mount Rainier. Thank you
-          for stopping by and please check out my portfolio of websites and
-          email campaigns I have developed, designed or&nbsp;both.
+          to tell you a little bit about myself, I&#39;m currently working as an
+          Email Developer for Vetsource. I have been a professional developer
+          for 7 years but I&#39;ve been developing software and building
+          PC&#39;s and electronics for the last 16 years. Front-end development
+          is my preferred workspace but I am capable of doing full stack work. I
+          follow the motto that you should work to live, not live to work and so
+          I try to find opportunities that support that lifestyle. When I&#39;m
+          not at work I love to play PC games and board games. Working from home
+          has allowed me to cultivate a growing collection of house plants. When
+          time permits I love to get out into the garage and start a new
+          woodworking project. If the weather is nice, you can find me outside
+          with my partner and dogs going on a hike, although, you won&#39;t find
+          me scaling Mount Rainier. Thank you for stopping by and please check
+          out my portfolio of websites and email campaigns I have developed,
+          designed or&nbsp;both.
         </div>
       </div>
       <div className="row skills-wrapper mb-5">
@@ -42,6 +42,7 @@ function Home() {
                 <li>AJAX/REST</li>
                 <li>Salesforce MC</li>
                 <li>Luminate</li>
+                <li>Marketo</li>
               </ul>
             </div>
             <div className="col-sm-6 col-xs-12">
@@ -52,6 +53,7 @@ function Home() {
                 <li>SQL</li>
                 <li>Responsys</li>
                 <li>Charity Engine</li>
+                <li>Iterable</li>
               </ul>
             </div>
           </div>

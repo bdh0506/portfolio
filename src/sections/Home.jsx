3a0c7f1272501bf0fa0248e425d09a41fdd366f5
bdh0506 <brandon0506@yahoo.com>
2026-03-25 22:07:22 -0700

    Update Home.jsx
    Update home page intro text

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,21 +9,21 @@ function Home() {
         </div>
         <div className="bio-text col-xl-6 col-lg-12">
           Hello, I&#39;m Brandon Harmon and I&#39;m a Senior Developer. Allow me
-          to tell you a little bit about myself, I&#39;m currently working as an
-          Email Developer for Vetsource. I have been a professional developer
-          for 7 years but I&#39;ve been developing software and building
-          PC&#39;s and electronics for the last 17 years. Front-end development
-          is my preferred workspace but I am capable of doing full stack work. I
-          follow the motto that you should work to live, not live to work and so
-          I try to find opportunities that support that lifestyle. When I&#39;m
-          not at work I love to play PC games and board games. Working from home
-          has allowed me to cultivate a growing collection of house plants. When
-          time permits I love to get out into the garage and start a new
-          woodworking project. If the weather is nice, you can find me outside
-          with my partner and dogs going on a hike, although, you won&#39;t find
-          me scaling Mount Rainier. Thank you for stopping by and please check
-          out my portfolio of websites and email campaigns I have developed,
-          designed or&nbsp;both.
+          to tell you a little bit about myself, I&#39;m currently working as a
+          Digital Automation Developer for Vetsource. I have been a professional
+          developer for 7 years but I&#39;ve been developing software and
+          building PC&#39;s and electronics for the last 17 years. Front-end
+          development is my preferred workspace but I am capable of doing full
+          stack work. I follow the motto that you should work to live, not live
+          to work and so I try to find opportunities that support that
+          lifestyle. When I&#39;m not at work I love to play PC games and board
+          games. Working from home has allowed me to cultivate a growing
+          collection of house plants. When time permits I love to get out into
+          the garage and start a new woodworking project. If the weather is
+          nice, you can find me outside with my partner and dogs going on a
+          hike, although, you won&#39;t find me scaling Mount Rainier. Thank you
+          for stopping by and please check out my portfolio of websites and
+          email campaigns I have developed, designed or&nbsp;both.
         </div>
       </div>
       <div className="row skills-wrapper mb-5">
